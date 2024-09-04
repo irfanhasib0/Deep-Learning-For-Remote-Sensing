7b0f50d5c33f5743f8bfd561f7c3d85bb0cfb523
irfanhasib0 <irfanhasib.me@gmail.com>
2024-09-05 00:08:47 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,2 @@
+import os
+os.system('wget https://earthengine.googleapis.com/v1/projects/earthengine-legacy/thumbnails/575011bfb1c92be029fb0ae49d131770-0a401503754ab8bd4183439006795969:getPixels -o aaa.tif')
